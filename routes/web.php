b4f1213367a71671f17402ce31d12d67e42b3cc9
Darko Ivkov <bVNyDUKE@protonmail.com>
2022-03-15 23:13:06 +0100

    add projects endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::group(['middleware' => ['auth', 'verified']], function(){
     Route::apiResources(['clients' => ClientController::class]);
+    Route::resource('projects', ProjectController::class);
     Route::get('/', fn() => Inertia::render('Dashboard'))->name('dashboard');
 });
 
